c6443811287802423d067de8dab45340c9492b61
antonhann <antonha016@gmail.com>
2025-10-17 14:55:23 -0400

    added log in page

---
+++ app/login/page.tsx
@@ -0,0 +1,56 @@
+"use client";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
+export default function Home() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const auth = getAuth();
+    const router = useRouter();
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try{
+            await signInWithEmailAndPassword(auth, email, password);
+            router.push("/")
+        }
+        catch(error){
+            console.error(error)
+        }
+    };
+
+
+    return (
+        <div>
+        <h1>This is the login page</h1>
+
+        <form onSubmit={handleSubmit}>
+            <div>
+            <label>Email:</label>
+            <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+            />
+            </div>
+
+            <div>
+            <label>Password:</label>
+            <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+            />
+            </div>
+
+            <button type="submit">Login</button>
+        </form>
+
+        <Link href="/">Home</Link>
+        </div>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 "use client";
 import { auth } from "@/lib/firebase";
+import Link from "next/link";
 
 export default function Home() {
-  console.log("Current Firebase user:", auth.currentUser);
-  return <div>Firebase initialized ✅</div>;
+  const currentUser = auth.currentUser
+  return (
+    <div>
+        <h1>This is the Home page</h1>
+        {currentUser && <p> Welcome {currentUser?.email}</p>}
+        <Link href = "/login">Login</Link>
+    </div>
+  );
+  
 }
